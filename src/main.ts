a5ed91adaa278dc394dcab6d5aa3f068f860a220
RestartFU <github@restartfu.com>
2025-08-06 15:12:56 -0400

    added single instance lock and refresh

--- src/main.ts
+++ src/main.ts
@@ -2,52 +2,72 @@ import { app, BrowserWindow } from 'electron';
 
 let win: BrowserWindow | null = null;
 
-function createWindow() {
-  win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    title: "AirGap Wallet",
-    backgroundColor: "#00000000",
-    titleBarStyle: 'hidden',
-    titleBarOverlay: {
-      color: '#00000000',
-      symbolColor: '#333333',
-      height: 24
-    },
-    webPreferences: {
-      contextIsolation: false,
-      nodeIntegration: false,
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (win) {
+      if (win.isMinimized()) win.restore();
+      win.focus();
     }
   });
 
-  win.loadURL('https://wallet.airgap.it');
-
-  win.setMenu(null);
-
-  win.webContents.on('did-finish-load', () => {
-    win!.webContents.insertCSS(`
-      body::before {
-        content: '';
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 24px;
-        background: transparent;
-        -webkit-app-region: drag;
-        z-index: 9999;
-        pointer-events: none;
+  function createWindow() {
+    win = new BrowserWindow({
+      width: 1200,
+      height: 800,
+      title: "AirGap Wallet",
+      backgroundColor: "#00000000",
+      titleBarStyle: 'hidden',
+      titleBarOverlay: {
+        color: '#00000000',
+        symbolColor: '#333333',
+        height: 24
+      },
+      webPreferences: {
+        contextIsolation: false,
+        nodeIntegration: false,
       }
+    });
 
-      body {
-        margin-top: 0px !important;
+    win.loadURL('https://wallet.airgap.it');
+
+    win.setMenu(null);
+
+    win.webContents.on('did-finish-load', () => {
+      win!.webContents.insertCSS(`
+        body::before {
+          content: '';
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 24px;
+          background: transparent;
+          -webkit-app-region: drag;
+          z-index: 9999;
+          pointer-events: none;
+        }
+
+        body {
+          margin-top: 0px !important;
+        }
+      `);
+    });
+
+    win.webContents.on('before-input-event', (event, input) => {
+      if ((input.control || input.meta) && input.key.toLowerCase() === 'r') {
+        win!.reload();
+        event.preventDefault();
       }
-    `);
+    });
+  }
+
+  app.whenReady().then(createWindow);
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
   });
 }
-
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
